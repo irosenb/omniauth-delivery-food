f002a1914c57170a9acfeb739ac2f829309fae70
Isaac Rosenberg <irosenb7@gmail.com>
2014-02-25 20:04:26 -0500

    added spec folder

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+Dir[File.expand_path('../support/**/*', __FILE__)].each { |f| require f }
+require 'bundler/setup'
+require 'rspec'
+require 'rack/test'
+require 'webmock/rspec'
+require 'omniauth'
+require 'omniauth-delivery'
+
+RSpec.configure do |config|
+  config.include WebMock::API
+  config.include Rack::Test::Methods
+  config.extend  OmniAuth::Test::StrategyMacros, :type => :strategy
+end
